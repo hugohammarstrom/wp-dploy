63ec37ebb20f073d26df531939dc6c1856f9e745
Hugo Hammarström <hugo@stateless.digital>
2019-01-01 21:57:56 +0100

    Added version to base command

--- src/commands/base.js
+++ src/commands/base.js
@@ -1,13 +1,16 @@
 import figlet from "figlet"
+const version = require("../../package.json").version
 
 export default function(program){
     figlet("wp dploy", function(err, data) {
         if (err) {
-            console.log('Something went wrong...');
+            console.log("Something went wrong...");
             console.dir(err);
             return;
         }
         console.log(global.chalk.blue(data))
+        console.log("")
+        console.log(global.chalk.blue("version:", version))
         console.log("")
         console.log("")
         program.outputHelp();
