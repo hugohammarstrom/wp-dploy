1381d4b3ff7313466afc923471c59cd82e1f8755
Hugo Hammarström <hugo@stateless.digital>
2019-01-04 13:14:52 +0100

    Added pulling of images on start

--- src/commands/start.js
+++ src/commands/start.js
@@ -2,26 +2,31 @@ import compose from "../docker-compose"
 import path from "path"
 import logger from "./../logger"
 import configHandler from "./../handlers/config"
+import exec from "../exec";
 
 
-export default function(a, b){
+export default async function(a, b){
     configHandler.loadConfig()
-    logger.createSpinner("dploy: start local wordpress environment")
-    compose.upAll({ cwd: path.join(process.cwd()), log: false })
-        .then((res) => { 
-            if (res.err && res.code !== 0){
-                logger.error("Something went wrong:", res.err)
-                logger.stop()
-            } else {
-                logger.success(global.chalk.green('dploy: started local wordpress environment'))
-                logger.stop()
-                console.log(global.chalk.yellow("dploy: it can take a couple of minutes if this is the first launch"))
-            }
-            process.exit(0)
-        }, 
-            err => { 
-                logger.error('Something went wrong:', err.message)
-                logger.stop()
-            }
-        );
+    
+    console.log(global.chalk.yellow("dploy: pulling images"))
+    logger.log("")
+    logger.stop()
+
+    await exec("docker-compose pull", {stderr: true})
+
+    logger.log("")
+    logger.success(global.chalk.green("dploy: pulled images"))
+
+    setTimeout(() => logger.info(global.chalk.yellow("dploy: starting local wordpress environment")))
+
+    let res = await compose.upAll({ cwd: path.join(process.cwd()), log: false })
+    
+    if (res.err && res.code !== 0){
+        logger.error("Something went wrong:", res.err)
+        logger.stop()
+    } else {
+        logger.success(global.chalk.green('dploy: started local wordpress environment'))
+        logger.stop()
+        console.log(global.chalk.yellow("dploy: it can take a couple of minutes if this is the first launch"))
+    }
 }
